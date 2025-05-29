56fd82cf96fd6e11fafe621418cce90565ef289b
Cyknow <cyknowtechservnigltd@gmail.com>
2025-05-29 12:54:05 +0100

    first commit

---
+++ react-app/src/Components/NavSection/Nav.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Nav.css";
+import bgimg from "../../Images/new.jpg";
+
+const Nav = () => {
+
+  return (
+    <div>
+      This is my first practice
+      <div className= "bgimg"> </div>
+    </div>
+  )
+}
+
+export default Nav
